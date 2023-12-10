46054b09dee4cea1abc1ba177528f49d55104ac8
Nojus <110340406+NoJuice1st@users.noreply.github.com>
2023-12-10 16:17:13 +0200

    Added new shopping system

--- Assets/Scripts/Pizza.cs
+++ Assets/Scripts/Pizza.cs
@@ -4,7 +4,32 @@ using UnityEngine;
 
 public class Pizza : MonoBehaviour
 {
-    private void OnMouseDown() {
+    public float rotateSpeed = 2f;
+    public float shrinkSpeed = 5f;
+    public Vector3 originalSize = Vector3.one * 1.5f;
+    public AudioSource source;
+    public ParticleSystem particles;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnMouseDown() 
+    {
         GameManager.clicks++;
+        transform.localScale = originalSize * 1.5f;
+
+        particles.Play();
+        source.PlayOneShot(source.clip);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.back * rotateSpeed * Time.deltaTime);
+        if(transform.localScale.x > 1.5f)
+        {
+            transform.localScale -= originalSize * shrinkSpeed * Time.deltaTime;
+        }
     }
 }
--- Assets/Scripts/Worker.cs
+++ Assets/Scripts/Worker.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Worker : MonoBehaviour
 {
@@ -9,8 +11,22 @@ public class Worker : MonoBehaviour
     public int price;
     public int cps;
 
+    public TMP_Text countText;
+    public TMP_Text priceText;
+    public Button button;
+
+    private void Update()
+    {
+        countText.text = count.ToString();
+        priceText.text = "price:" + price;
+
+        var canClick = GameManager.clicks >= price;
+        button.interactable = canClick;
+    }
+
     private void Start() 
     {
+        InvokeRepeating("Work", 1f, 1f);
         Load();
     }
 
@@ -19,17 +35,25 @@ public class Worker : MonoBehaviour
         if (GameManager.clicks < price) return;
 
         GameManager.clicks -= price;
+        price = (int)(price * 1.4);
         count++;
         Save();
+    }
+
+    public void Work()
+    {
+        GameManager.clicks += count * cps;
     }
 
     void Save()
     {
         PlayerPrefs.SetInt(workerName, count);
+        PlayerPrefs.SetInt(name + "price", price);
     }
 
     void Load()
     {
         count = PlayerPrefs.GetInt(workerName);
+        price = PlayerPrefs.GetInt(name + "price", price);
     }
 }
